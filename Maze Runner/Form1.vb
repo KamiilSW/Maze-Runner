a00b7bdda30ca45802b584ec45861d90b2fe6223
Kamil Swierkosz <kam.swierkosz@gmail.com>
2024-09-12 14:57:41 +0100

    Add project files.

---
+++ Maze Runner/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
