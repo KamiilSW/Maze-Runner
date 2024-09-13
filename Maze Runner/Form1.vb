2e5e04de8a15d404cd49282992786c91a5d010da
KamiilSW <kam.swierkosz@gmail.com>
2024-09-13 16:42:47 +0100

    Exit is created

--- Maze Runner/Form1.vb
+++ Maze Runner/Form1.vb
@@ -2,53 +2,54 @@
     Dim gridSize As Integer = 50 'Amount of rows and columns!
     Dim cellSize As Integer = 10 ' Size of cells!
     Private Panel1 As New Panel()
+    Private CellsOnEdge As New List(Of PictureBox) ' List to hold edge cells
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Panel1.Location = New Point(10, 10) ' Puts the panels location at this point
+        Panel1.Location = New Point(10, 10) ' Puts the panel's location at this point
         Panel1.Size = New Size(gridSize * cellSize, gridSize * cellSize)
-        Me.Controls.Add(Panel1) 'Adds the panel to the form
+        Me.Controls.Add(Panel1)
 
         For i As Integer = 0 To gridSize - 1
             For j As Integer = 0 To gridSize - 1
                 Dim cell As New PictureBox()
 
-                'Cell properties created
+                ' Gives the cells properties
                 cell.Size = New Size(cellSize, cellSize)
                 cell.Location = New Point(j * cellSize, i * cellSize)
                 cell.BackColor = Color.DarkSlateGray
 
-                AddHandler cell.BackColorChanged, AddressOf PictureBox_BackColorChanged
+                ' Adds the PictureBox to the panel controls.
                 Panel1.Controls.Add(cell)
+
+                ' Add edge cells to the list
+                If i = 0 OrElse i = gridSize - 1 OrElse j = 0 OrElse j = gridSize - 1 Then
+                    CellsOnEdge.Add(cell)
+                End If
+
                 FindMiddle(cell)
-                CreateExit(cell)
             Next
         Next
+        CreateExit()
     End Sub
-    Sub FindMiddle(ByRef cell)
-        Dim middle As Integer = (gridSize / 2) - 1 ' Finds the middle
 
+    ' CHanges the middle cell's colour.
+    Sub FindMiddle(ByRef cell As PictureBox)
+        Dim middle As Integer = (gridSize / 2) - 1 ' Finds the middle
         If cell.Location = New Point(middle * cellSize, middle * cellSize) Then
-            cell.BackColor = Color.White 'If the cell is in the middle location it changes the colour to white, this is the starting point
+            cell.BackColor = Color.White ' Middle cell changes color to white
         End If
     End Sub
 
-    Sub CreateExit(ByRef cell)
+    ' Changes one of the random cells on the edge.
+    Sub CreateExit()
         Dim random As New Random()
-        Dim randNumber As Integer = random.Next(0, gridSize)
-        Dim randGridNumber As Integer = random.Next(0, gridSize)
-        Dim edgeCoord As Integer = ((randGridNumber) - 1) * randNumber
-        Dim edgeCounter As Integer = 0
-
-        For i = 1 To randNumber
-            If i = randNumber Then
-                If cell.Location.Y = edgeCoord * cellSize Or cell.Location.X = edgeCoord * cellSize Then
-                    cell.BackColor = Color.White
-                    Exit For
-                End If
-            End If
-        Next
+        ' Randomly select one cell from the edgeCells list
+        Dim randomEdgeCell As PictureBox = CellsOnEdge(random.Next(CellsOnEdge.Count))
+        ' Change the selected edge cell's color to white
+        randomEdgeCell.BackColor = Color.White
     End Sub
+
     Private Sub PictureBox_BackColorChanged(control As Object, e As EventArgs)
-
+        ' Handle background color change if necessary
     End Sub
-
 End Class
